699c25c479ef59e98f1263cc4237906f0a19315f
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-15 01:31:50 +0100

    I rewrited whole js file, added space to screen to display current operator

--- script.js
+++ script.js
@@ -1,107 +1,87 @@
-let action = "";
-let num2 = [];
-let num1 = 0;
-let displayValue = 0;
-var buttonNumber = document.querySelectorAll(".btn.nbr");
-var display = document.querySelector(".display");
-var buttonSpecial  = document.querySelectorAll(".btn.special");
-// BUTTONS
+let enteredNumbers = "";
+let results;
+let display = "";
+let operatorDisplay = "";
+let temp;
+let iteration = 0;
 
-setInterval(refresh, 10);
+var buttonNumber = document.querySelectorAll(".btn.nbr");
+var num = document.querySelector(".num");
+var operator = document.querySelector(".operator");
+var buttonSpecial  = document.querySelectorAll(".btn.special.fn");
+var del = document.querySelector(".btn.special.del");
+var equal = document.querySelector(".btn.special.eq");
 
 for (let i = 0; i < buttonNumber.length; i++) {
     buttonNumber[i].addEventListener("click", function (e) {
-        let contents = buttonNumber[i].innerHTML;
-        num2.push(contents)
-        displayValue = Number(num2.join().replace(/,/g, ''))
+        enteredNumbers += buttonNumber[i].innerHTML;
+        display = enteredNumbers;
         })
     }
 for (let j = 0; j < buttonSpecial.length; j++) {
     buttonSpecial[j].addEventListener("click", function (e) {
-        if (buttonSpecial[j].innerHTML == "+") {
-            plus();
-        }else if (buttonSpecial[j].innerHTML == "-") {
-            minus();
-        }else if (buttonSpecial[j].innerHTML == "*") {
-            mltp();
-        }else if (buttonSpecial[j].innerHTML == "/") {
-            dvd();
-        }else if (buttonSpecial[j].innerHTML == "C") {
-            reset()
-        }else if (buttonSpecial[j].innerHTML == "=") {
-            execute();
+        if (iteration == 0) {
+            enteredNumbers = Number(enteredNumbers);
+            temp = enteredNumbers;
+            enteredNumbers = "";
+            operatorDisplay = buttonSpecial[j].innerHTML;
+            display = temp;
+            iteration += 1;
+        }else {
+            enteredNumbers = Number(enteredNumbers);
+            operate(temp, enteredNumbers);
+            display = results;
+            temp = results;
+            enteredNumbers = "";
+            operatorDisplay = buttonSpecial[j].innerHTML;
+
         }
-        
-    })
+    });
 }
+del.addEventListener("click", function (e) {
+enteredNumbers = "";
+results = 0;
+display = "";
+operatorDisplay = "";
+temp = 0;
+iteration = 0;
+});
+equal.addEventListener("click", function (e) {
+if (iteration == 0){
+    return;
+}else {
+    enteredNumbers = Number(enteredNumbers);
+    operate(temp, enteredNumbers);
+    display = results;
+}
+})
+// FUNCTIONS
 
-// function 
-
+function operate(temp, enteredNumbers) {
+    if (operatorDisplay == "+") {
+        results = adding(temp,enteredNumbers); 
+   } else if (operatorDisplay == "-") {
+    results = susbstracting(temp, enteredNumbers);
+   }else if (operatorDisplay == "*") {
+    results = multiply(temp, enteredNumbers);
+   }else if (operatorDisplay == "/") {
+    results = divide(temp, enteredNumbers);
+   }
+}
 function adding(a, b) {
-   let result =  a + b;
-   return result
+    return a + b;
 }
 function susbstracting(a, b) {
-    let result = a- b
-    return result
+    return a - b;   
 }
-function multiply (a, b) {
-    let result = a * b;
-    return result
+function multiply(a, b) {
+    return a * b;
 }
-function divide (a, b) {
-    let result = a / b;
-    return result
+function divide(a, b) {
+    return a / b;
 }
-function operate(num1, num2, action) {
-     if (action == "plus") {
-        displayValue = adding(num1,num2);
-      return adding(num1,num2)    
-    } else if (action == "minus") {
-        displayValue =susbstracting;
-      return susbstracting(num1, num2)
-    }else if (action == "mltp") {
-        displayValue = multiply(num1, num2);
-      return multiply(num1, num2)
-    }else if (action == "dvd") {
-        displayValue = divide(num1, num2);
-      return divide(num1, num2)
-    }
-}
-function switcher() {
-num1 = Number(num2.join().replace(/,/g, ''));
-num2 = [];
-}
+setInterval(refresh, 10);
 function refresh() {
-    display.innerHTML = displayValue;
-}
-function plus() {
-    action = "plus";
-    displayValue = "+";
-    switcher();
-}
-function minus() {
-    action = "minus";
-    displayValue = "-"
-    switcher();
-}
-function mltp() {
-    action = "mltp";
-    displayValue = "*"
-    switcher();
-}
-function dvd() {
-    action = "dvd";
-    displayValue = "/"
-    switcher();
-}
-function reset() {
-    action = "";
-    num2 = [];
-    num1 = 0;
-    displayValue = 0;
-}
-function execute() {
-    num2 = Number(num2.join().replace(/,/g, ''));
-    operate(num1, num2, action);
+    num.innerHTML = display;
+    operator.innerHTML = operatorDisplay;
 }
