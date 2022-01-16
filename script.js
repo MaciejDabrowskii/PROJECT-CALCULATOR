8487269a33f118dc3993ff9a5107859bff7fb26b
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-16 11:09:07 +0100

    I added full numerical keyboard support, cleared code, changed some of functions to arrow functions. I changed way the event Listener is applied to divs, from loop to forEach method

--- script.js
+++ script.js
@@ -9,49 +9,81 @@ var buttonNumber = document.querySelectorAll(".btn.nbr");
 var num = document.querySelector(".num");
 var operator = document.querySelector(".operator");
 var buttonSpecial  = document.querySelectorAll(".btn.special.fn");
-var del = document.querySelector(".btn.special.del");
+var del = document.querySelector(".btn.del");
 var equal = document.querySelector(".btn.special.eq");
 var btns = document.querySelectorAll(".btn");
 var bspc = document.querySelector(".bspc");
 
-
-setInterval(refresh, 10);
-
-bspc.addEventListener("click", function (e) {
-    if (enteredNumbers == "") {
-        return
-    }else {
-        enteredNumbers = enteredNumbers.slice(0,-1);
+// Functions handling mouse clicks on divs
+bspc.addEventListener("click", (e) => {
+        backspace();
+    });
+del.addEventListener("click", (e) => {
+        deletion();
+    });
+equal.addEventListener("click", (e) => {
+        equalisation();
+    });
+buttonNumber.forEach(btn => btn.addEventListener("click", (e) => {
+        enteredNumbers += e.target.innerHTML;
         display = enteredNumbers;
-    }
-});
-for (let i = 0; i < buttonNumber.length; i++) {
-    buttonNumber[i].addEventListener("click", function (e) {
-        enteredNumbers += buttonNumber[i].innerHTML;
-        display = enteredNumbers;
-        })
-    }
-for (let j = 0; j < buttonSpecial.length; j++) {
-    buttonSpecial[j].addEventListener("click", function (e) {
+    })
+);
+buttonSpecial.forEach(btn => btn.addEventListener("click", (e) => {
         if (iteration == 0) {
             enteredNumbers = Number(enteredNumbers);
             temp = enteredNumbers;
             enteredNumbers = "";
-            operatorDisplay = buttonSpecial[j].innerHTML;
+            operatorDisplay = e.target.innerHTML;
             display = temp;
             iteration += 1;
-        }else if (enteredNumbers == "") {
-            operatorDisplay = buttonSpecial[j].innerHTML;
-        }else {
+        } else if (enteredNumbers == "") {
+            operatorDisplay = e.target.innerHTML;
+        } else {
             enteredNumbers = Number(enteredNumbers);
             operate(temp, enteredNumbers);
             display = results;
             temp = results;
             enteredNumbers = "";
-            operatorDisplay = buttonSpecial[j].innerHTML;
-        }
-    });
-};
+            operatorDisplay = e.target.innerHTML;
+        };
+    })
+)
+// Function handling keyboard support
+window.addEventListener('keydown', (event) => {
+    const keySpecial = document.querySelector(`.special[data-key="${event.code}"]`);
+    const keyNbr = document.querySelector(`.nbr[data-key="${event.code}"]`);
+    const keyBspc = document.querySelector(`.bspc[data-key="${event.code}"]`);
+    const keyDel = document.querySelector(`.del[data-key="${event.code}"]`);
+    console.log(keySpecial)
+    if (keySpecial !== null) {
+        if (iteration == 0) {
+            enteredNumbers = Number(enteredNumbers);
+            temp = enteredNumbers;
+            enteredNumbers = "";
+            operatorDisplay = keySpecial.innerHTML;
+            display = temp;
+            iteration += 1;
+        } else if (enteredNumbers == "") {
+            operatorDisplay = keySpecial.innerHTML;
+        } else {
+            enteredNumbers = Number(enteredNumbers);
+            operate(temp, enteredNumbers);
+            display = results;
+            temp = results;
+            enteredNumbers = "";
+            operatorDisplay = keySpecial.innerHTML;
+        };
+    }else if (keyNbr !== null) {
+        enteredNumbers += keyNbr.innerHTML;
+        display = enteredNumbers;
+    }else if (keyBspc !== null) {
+        backspace();
+    }else if (keyDel !== null) {
+        deletion();
+    }
+});
+// Functions adding style on click
 btns.forEach(btn => btn.addEventListener("mousedown", (e) => {
     btn.classList.add("pushed")
 }));
@@ -64,16 +96,15 @@ bspc.addEventListener("mousedown", (e) => {
 bspc.addEventListener("mouseup", (e) => {
     bspc.classList.remove("pushed")
 });
-
-del.addEventListener("click", function (e) {
-    deletion();
-});
-equal.addEventListener("click", function (e) {
-    equalisation();
-});
-
-// FUNCTIONS
-
+// Button functions
+function backspace() {
+    if (enteredNumbers == "") {
+        return
+    }else {
+        enteredNumbers = enteredNumbers.slice(0,-1);
+        display = enteredNumbers;
+    }
+};
 function equalisation() {
     if (iteration == 0){
         return;
@@ -87,11 +118,11 @@ function equalisation() {
 };
 function deletion() {
     enteredNumbers = "";
-results = 0;
-display = "";
-operatorDisplay = "";
-temp = 0;
-iteration = 0;
+    results = 0;
+    display = "";
+    operatorDisplay = "";
+    temp = 0;
+    iteration = 0;
 };
 function operate(temp, enteredNumbers) {
     if (operatorDisplay == "+") {
@@ -104,6 +135,7 @@ function operate(temp, enteredNumbers) {
     results = divide(temp, enteredNumbers);
    }
 };
+// Arithmetic functions
 function adding(a, b) {
     return a + b;
 };
@@ -120,6 +152,8 @@ function divide(a, b) {
     return (a / b);
     }
 };
+//Screen refreshing function
+setInterval(refresh, 10);
 function refresh() {
     num.innerHTML = display;
     operator.innerHTML = operatorDisplay;
